eb6e78d33cb133e62c8d186c7e571fd6d46fe5f3
mikseros <mmadynski@gmail.com>
2022-06-13 22:19:16 +0200

    Initial commit

---
+++ src/main/java/com/mikseros/usermanagerapp/UsermanagerappApplication.java
@@ -0,0 +1,13 @@
+package com.mikseros.usermanagerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanagerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanagerappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikseros/usermanagerapp/UsermanagerappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikseros.usermanagerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
