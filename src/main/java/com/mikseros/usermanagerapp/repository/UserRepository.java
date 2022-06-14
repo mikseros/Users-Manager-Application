2ff4feca591f0e0dcef4dfb608d4b67a2bae4cab
mikseros <mmadynski@gmail.com>
2022-06-14 11:22:34 +0200

    User Model (@Entity) + User Repository (CRUD)

---
+++ src/main/java/com/mikseros/usermanagerapp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.mikseros.usermanagerapp.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.mikseros.usermanagerapp.model.User;
+
+public interface UserRepository extends CrudRepository<User, Integer>{
+
+}
