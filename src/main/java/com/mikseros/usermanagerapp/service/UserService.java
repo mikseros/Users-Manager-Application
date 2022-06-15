f084e11f0b58d76d8445a4cd12d44bc98d980139
mikseros <mmadynski@gmail.com>
2022-06-15 22:43:47 +0200

    Users Listing Page -> UserService

---
+++ src/main/java/com/mikseros/usermanagerapp/service/UserService.java
@@ -0,0 +1,19 @@
+package com.mikseros.usermanagerapp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mikseros.usermanagerapp.model.User;
+import com.mikseros.usermanagerapp.repository.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> listAll() {
+		return (List<User>) userRepository.findAll();
+	}
+}
