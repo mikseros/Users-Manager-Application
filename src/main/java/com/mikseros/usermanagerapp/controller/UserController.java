d90d1fb0f1ee64a5b41e32326a0efe5ccb98784c
mikseros <mmadynski@gmail.com>
2022-06-15 22:45:07 +0200

    Users Listing Page -> UserController

---
+++ src/main/java/com/mikseros/usermanagerapp/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.mikseros.usermanagerapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.mikseros.usermanagerapp.model.User;
+import com.mikseros.usermanagerapp.service.UserService;
+
+@Controller
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/users")
+	public String showUserList(Model model) {
+		List<User> listUsers = userService.listAll();
+		model.addAttribute("listUsers", listUsers);
+		return "users";
+	}
+}
