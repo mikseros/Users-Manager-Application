0619b3b79cce752f3921e33f83547a31597a3bca
mikseros <mmadynski@gmail.com>
2022-06-16 23:40:51 +0200

    Add User -> Controller modification

--- src/main/java/com/mikseros/usermanagerapp/controller/UserController.java
+++ src/main/java/com/mikseros/usermanagerapp/controller/UserController.java
@@ -22,4 +22,10 @@ public class UserController {
 		model.addAttribute("listUsers", listUsers);
 		return "users";
 	}
+	
+	@GetMapping("/users/new")
+	public String showNewForm(Model model) {
+		model.addAttribute("user", new User());
+		return "user_form";
+	}
 }
