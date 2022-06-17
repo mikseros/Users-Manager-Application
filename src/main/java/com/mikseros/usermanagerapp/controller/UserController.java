add82be6c7f3f4ff2c8dd3ad9065cbff5280fbaa
mikseros <mmadynski@gmail.com>
2022-06-17 13:39:06 +0200

    Add New User -> save() @PostMapping; Controller

--- src/main/java/com/mikseros/usermanagerapp/controller/UserController.java
+++ src/main/java/com/mikseros/usermanagerapp/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.mikseros.usermanagerapp.model.User;
 import com.mikseros.usermanagerapp.service.UserService;
@@ -27,5 +28,11 @@ public class UserController {
 	public String showNewForm(Model model) {
 		model.addAttribute("user", new User());
 		return "user_form";
+	}
+	
+	@PostMapping("/users/save")
+	public String saveUser(User user) {
+		userService.save(user);
+		return "redirect:/users";
 	}
 }
