90c637db2f04fb23a1c40a8f9d53c6287d743e43
mikseros <mmadynski@gmail.com>
2022-06-18 16:47:48 +0200

    Add New User -> User Added Message

--- src/main/java/com/mikseros/usermanagerapp/controller/UserController.java
+++ src/main/java/com/mikseros/usermanagerapp/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.mikseros.usermanagerapp.model.User;
 import com.mikseros.usermanagerapp.service.UserService;
@@ -31,8 +32,9 @@ public class UserController {
 	}
 	
 	@PostMapping("/users/save")
-	public String saveUser(User user) {
+	public String saveUser(User user, RedirectAttributes ra) {
 		userService.save(user);
+		ra.addFlashAttribute("message", "The user has been saved successfully");
 		return "redirect:/users";
 	}
 }
