7c660733ef77abce20fb6c8f83ba2a3c8ccac80d
mikseros <mmadynski@gmail.com>
2022-06-18 17:10:14 +0200

    Edit User -> Business function getById()

--- src/main/java/com/mikseros/usermanagerapp/service/UserService.java
+++ src/main/java/com/mikseros/usermanagerapp/service/UserService.java
@@ -1,10 +1,12 @@
 package com.mikseros.usermanagerapp.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mikseros.usermanagerapp.exception.UserNotFoundException;
 import com.mikseros.usermanagerapp.model.User;
 import com.mikseros.usermanagerapp.repository.UserRepository;
 
@@ -19,5 +21,14 @@ public class UserService {
 
 	public void save(User user) {
 		userRepository.save(user);
+	}
+	
+	public User get(Integer id) throws UserNotFoundException {
+		Optional<User> result = userRepository.findById(id);
+		if (result.isPresent()) {
+			return result.get();
+		} else {
+			throw new UserNotFoundException("User with id: " + id + " not found!");
+		}
 	}
 }
