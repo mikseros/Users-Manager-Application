776bf38c82d69d9fa2ee5dee9a60d330bc3b334f
mikseros <mmadynski@gmail.com>
2022-06-18 17:08:46 +0200

    Edit User -> UserNotFoundException

---
+++ src/main/java/com/mikseros/usermanagerapp/exception/UserNotFoundException.java
@@ -0,0 +1,10 @@
+package com.mikseros.usermanagerapp.exception;
+
+public class UserNotFoundException extends Throwable {
+	private static final long serialVersionUID = 1L;
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+	
+}
