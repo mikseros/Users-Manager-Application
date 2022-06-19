32315d089c18a8efb161b43f09f8e9a92cc5f327
mikseros <mmadynski@gmail.com>
2022-06-19 17:01:16 +0200

    Delete User -> new function in repository

--- src/main/java/com/mikseros/usermanagerapp/repository/UserRepository.java
+++ src/main/java/com/mikseros/usermanagerapp/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.mikseros.usermanagerapp.model.User;
 
 public interface UserRepository extends CrudRepository<User, Integer>{
-
+	public Long countById(Integer id);
 }
